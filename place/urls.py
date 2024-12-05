e1b72173caa9e2b2933e51bcf0a4a974d335f9ac
espionage23 <zalstmd23@naver.com>
2024-12-05 18:16:03 +0900

    feat: 상세페이지 제작(미완성)

--- place/urls.py
+++ place/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'place'
 urlpatterns = [
     path('local/<str:areacode>/', views.local, name='local'),
+    path('view/<str:areacode>/', views.view, name='view'),
 ]
--- place/views.py
+++ place/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def local(request, areacode):
   context = {'areacode': areacode}
   return render(request, 'local.html', context)
+
+
+def view(request, areacode):
+  context = {'areacode': areacode}
+  return render(request, 'view.html', context)
+  
