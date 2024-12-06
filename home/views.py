ab2942d5416cfe4d7786efea48731c911bb50b39
jihoon <jeelee553@gmail.com>
2024-12-06 17:02:42 +0900

    Feature: animate area list at index.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from areacode.models import AreaCode
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  area_codes = AreaCode.objects.all()
+
+  return render(request, 'index.html', context={"area_codes": area_codes})
