0473f00f5f204715727b3d503ffce3ae89eeaa9c
WonYoung <ayg1044@naver.com>
2024-12-09 17:57:53 +0900

    2024-12-09 local.html에 데이터 입력하기, 및 페이징 처리

--- place/urls.py
+++ place/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'place'
+
 urlpatterns = [
     path('local/<str:areacode>/', views.local, name='local'),
+    path('test/', views.test, name='test'),
 ]
--- place/views.py
+++ place/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from place.models import Place
 
 # Create your views here.
 def local(request, areacode):
-  context = {'areacode': areacode}
+  tour_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=12)
+  event_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=15)
+  context = {
+    'areacode': areacode,
+    'tour_list': tour_list,
+    'event_list': event_list
+  }
   return render(request, 'local.html', context)
+
+def test(request):
+  areacode = request.GET.get('id')
+  tour_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=12)
+  event_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=15)
+  context = {
+    'areacode': areacode,
+    'tour_list': tour_list,
+    'event_list': event_list
+  }
+  return render(request, 'test.html', context)
