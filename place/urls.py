f451068d2e1590f4b2767b04a7efae94e77bc768
espionage23 <zalstmd23@naver.com>
2024-12-10 12:24:09 +0900

    feat : url 경로 수정

--- place/urls.py
+++ place/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'place'
 urlpatterns = [
     path('local/<str:areacode>/', views.local, name='local'),
-    path('<str:areacode>/view/<str:content_id>', views.view, name='view'),
+    path('local/<str:areacode>/view/<str:content_id>', views.view, name='view'),
     path('test/', views.test, name='test'),
 ]
