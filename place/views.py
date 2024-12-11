294c6bc82daed5597c7cd3e0ca6903a67b976658
espionage23 <zalstmd23@naver.com>
2024-12-11 09:53:11 +0900

    re:views.py 수정

--- place/views.py
+++ place/views.py
@@ -11,14 +11,12 @@ from api import api
 
 # Create your views here.
 def local(request, areacode):
-  tour_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=12).order_by('-thumb_img', '-updated_at')
-  event_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=15).order_by('-thumb_img', '-start_time')
-  suggestion_tour = tour_list[:4]
-  suggestion_event = event_list[:3]
+  tour_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=12).order_by('-thumb_img', '-updated_at')[:4]
+  event_list = Place.objects.all().filter(sigungu_code__area_code=areacode, category__content_type=15).order_by('-thumb_img', '-start_time')[:3]
   context = {
     'areacode': areacode,
-    'tour_list': suggestion_tour,
-    'event_list': suggestion_event
+    'tour_list': tour_list,
+    'event_list': event_list
   }
   return render(request, 'local.html', context)
 
