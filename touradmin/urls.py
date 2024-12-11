1237c020fe836467a4d446823beff0169fc5be6c
WonYoung <ayg1044@naver.com>
2024-12-11 14:33:10 +0900

    adminpage

--- touradmin/urls.py
+++ touradmin/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'touradmin'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.touradmin, name='index'),
+    path('get_place/', views.get_place, name='get_place'),
+    path('get_event/', views.get_event, name='get_event'),
 ]
