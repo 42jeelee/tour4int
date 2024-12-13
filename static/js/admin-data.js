f9efeaaa06d3144b1319406717805745e35ad22e
jihoon <jeelee553@gmail.com>
2024-12-13 13:42:47 +0900

    Fix: fetch api_status before settingAllBox at admin-data

--- static/js/admin-data.js
+++ static/js/admin-data.js
@@ -135,6 +135,8 @@ $(function() {
       success: function(data) {
         const { result, log_info: logInfo } = data;
         
+        api_status[dataName] = false;
+        loadingItem(item, dataName);
         if (result === "success") settingAllBox(logInfo);
         else {
           alert("서버에 문제가 발생하였습니다.");
@@ -142,11 +144,8 @@ $(function() {
         }
       },
       error: function(e) {
-        console.log("fail :", e);
-      },
-      complete: function() {
         api_status[dataName] = false;
-        loadingItem(item, dataName);
+        console.log("fail :", e);
       }
     });
   }
