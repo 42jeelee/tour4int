a9fcb143212500a7ea279f9a91ab59ae1fed52d9
jihoon <jeelee553@gmail.com>
2024-12-13 18:14:01 +0900

    Feature: add column place
    - add column place model

--- api/urls.py
+++ api/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('init_sigungucode/', views.init_sigungucode, name='init_sigungucode'),
     path('get_place/', views.get_place, name='get_place'),
     path('get_event/', views.get_event, name='get_event'),
+    path('get_place_info/<str:contentid>', views.get_place_info, name='get_place_info'),
     path('get_logged/', views.get_logged, name='get_logged'),
 ]
