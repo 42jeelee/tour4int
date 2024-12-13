06beef2bef33d41852131c0eaf428ad0f0bf56bc
jihoon <jeelee553@gmail.com>
2024-12-13 13:14:52 +0900

    Feature: add fixed headerbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,22 @@
 $(function() {
-  $("#connect-js > span").text("ON").css("color", "green");
+  const headerBar = $("header > .header__container:first-child > .header__container--wrap");
+  
+  let lastScrollTime = 0;
+  let throttleDelay = 10;
+
+  $(window).on('scroll', function() {
+    const now = new Date().getTime();
+    if (now - lastScrollTime >= throttleDelay) {
+      lastScrollTime = now;
+      const currTop = $(window).scrollTop();
+
+      if (currTop > 500) headerBar.addClass("search-header");
+      else headerBar.removeClass("search-header");
+      if (currTop > 60) {
+        headerBar.addClass("fixed-top");
+      } else {
+        headerBar.removeClass("fixed-top");
+      }
+    }
+  });
 });
