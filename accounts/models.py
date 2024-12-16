913a2cba2ff0e2d195bcc8295cae5baeeed9248d
jihoon <jeelee553@gmail.com>
2024-12-16 15:36:33 +0900

    Refactor: AbstractUser -> AbstractBaseUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,30 +1,88 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+from place.models import Place
+from .managers import UserManager
 import random
 import string
 
-class User(AbstractUser):
+
+def validate_place_ids(value):
+
+    if value is None or len(value) == 0: return
+
+    place_ids = value.split(',')
+
+    if len(place_ids) > 10:
+        raise ValidationError("place_ids cannot exceed 10.")
+
+    db_places = Place.objects.filter(id__in=place_ids).values_list('id', flat=True)
+    diff = set(place_ids) - db_places
+
+    if len(diff) > 0:
+        raise ValidationError(f"place_id({', '.join(diff)}) is not exist.")
+
+
+class User(AbstractBaseUser):
     """
     사용자 모델
     username 필드: 이메일 주소로 사용
     email 필드: username과 동일한 이메일 주소
     password 필드: Django 기본 password 필드 사용
     """
-    name = models.CharField('이름', max_length=10)
-    nickname = models.CharField('닉네임', max_length=10)
-    address = models.CharField('주소', max_length=100)
-    created_at = models.DateTimeField('생성일', auto_now_add=True)
-    updated_at = models.DateTimeField('수정일', auto_now=True)
-    is_email_verified = models.BooleanField('이메일 인증 여부', default=False)
+    email = models.EmailField('이메일', unique=True, blank=False)
+    name = models.CharField('이름', max_length=10, blank=False)
+    nickname = models.CharField('닉네임', max_length=10, blank=False)
+    address = models.CharField('주소', max_length=100, null=True, blank=True)
 
+    is_email_verified = models.BooleanField('이메일 인증 여부', default=False)
+    is_staff = models.BooleanField('스탭여부', default=False)
+    is_superuser = models.BooleanField('관리자여부', default=False)
+    is_active = models.BooleanField('활성여부', default=False)
+
+    updated_at = models.DateTimeField('수정일', auto_now=True)
+    created_at = models.DateTimeField('생성일', auto_now_add=True)
+
+    place_history = models.CharField(
+        '방문기록',
+        max_length=120,
+        validators=[validate_place_ids],
+        help_text= '","로 구분하는 place_id (최대 10개)',
+        null=True,
+        blank=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'nickname']
+    objects = UserManager()
+
+    def add_place_history(self, place_id):
+        if self.place_history:
+            place_ids = self.place_history.split(',')
+
+            if place_id not in place_ids:
+                place_ids.insert(0, place_id)
+
+                if len(place_ids) > 10: place_ids.pop()
+
+                self.place_history = ','.join(place_ids)
+        else:
+            self.place_history = place_id
+
+        self.save()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+    
     class Meta:
         db_table = 'user'
         verbose_name = '사용자'
         verbose_name_plural = '사용자'
-
-    def __str__(self):
-        return f"{self.name} ({self.email})"
 
 class EmailVerification(models.Model):
     """
